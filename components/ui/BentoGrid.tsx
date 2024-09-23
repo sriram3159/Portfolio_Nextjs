d39657d7c3cd894a0bf2a0b4f59e26645609d376
sriram3159 <sriram3159@gmail.com>
2024-09-23 11:29:24 +0530

    Bug fixed

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -83,7 +83,7 @@ export const BentoGridItem = ({
           <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2 text-lg lg:text-3xl max-w-96 z-10">
             {title}
           </div>
-          {/* {id === 2 && <GlobeDemo />} */}
+          {id === 2 && <GlobeDemo />}
           {id === 3 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
               <div className="flex flex-col gap-3 lg:gap-8">
