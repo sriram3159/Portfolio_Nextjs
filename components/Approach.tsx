48fba6131d6c0e1484f645e85add77b0200caa43
sriram3159 <sriram3159@gmail.com>
2024-11-01 17:44:16 +0530

    xdcf

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -8,7 +8,7 @@ export const Approach = () => {
   return (
     <section className="w-full py-20">
       <h1 className="heading">
-        My  <span className="text-purple">approach</span>
+        My  <span className="text-purple">Approa</span>
       </h1>
       <div className="my-20 flex flex-col lg:flex-row items-center justify-center  gap-4">
         <Card
